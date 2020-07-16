519ed69f5db34ceab4b22b6fdeb5ecd90bfde574
Juho Timonen <juho.timonen@aalto.fi>
2020-07-16 23:09:03 +0300

    add corrected frontend url

--- build/precache-manifest.4586dc3fb97bde2542b2d10a1a791840.js
+++ build/precache-manifest.4586dc3fb97bde2542b2d10a1a791840.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "120f7b7dba59e26b7448992fdaddd8a7",
+    "revision": "acacfc1771b8001c6ec0ce1db4523921",
     "url": "/index.html"
   },
   {
-    "revision": "871e0f4bd5b5a5f90cd7",
+    "revision": "e84d2d9098d60568549c",
     "url": "/static/css/main.ba927a3e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "871e0f4bd5b5a5f90cd7",
-    "url": "/static/js/main.31fae2db.chunk.js"
+    "revision": "e84d2d9098d60568549c",
+    "url": "/static/js/main.a20a1c51.chunk.js"
   },
   {
     "revision": "f4d2ac87a76b6331fd91",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d60a3c906ee044ef4dd886668360332a.js"
+  "/precache-manifest.4586dc3fb97bde2542b2d10a1a791840.js"
 );
 
 self.addEventListener('message', (event) => {
